3b124792d7ebdd2b7f76c6c4e277691ac8b55de8
ashleighmm <ashleigh@hoistdigital.co.uk>
2020-05-02 16:04:07 +0100

    Added reduce to ingredients array to determine the length, find out if there are any ingredients or default message displays

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import classes from "./Burger.css";
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     // transform an object of key pair values (from state BurgerBuilder) into an array of burger ingredients where the value of the object is important to decide how many ingredients are needed, the key is important for the type of ingredient specified
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
         return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
             return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
         });
-    });
+    }) // reduce flattens the array into a single value
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+    // By adding reduce method, can get the length of the array according to the value set in each state ingredient, instewad of the array having empty elements, in which case you would always have a length of 4 even if set to 0 (salad:0,meat:0,cheese:0,bacon:0)
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please pick some ingredients!</p>
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
