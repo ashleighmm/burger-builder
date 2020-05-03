f1254378ca3b7a896fddb163f7c3e8bf8c378083
ashleighmm <ashleigh@hoistdigital.co.uk>
2020-05-03 16:06:15 +0100

    Display Modal with Order Summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Aux from './../../../hoc/Aux';
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return <li key={ingredientKey}>
+                <span style={{textTransform:'capitalize'}}>
+                    {ingredientKey}</span>: {props.ingredients[ingredientKey]}
+                </li>
+        })
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>Your burger contains the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Aux>
+    )
+}
+
+export default orderSummary;
