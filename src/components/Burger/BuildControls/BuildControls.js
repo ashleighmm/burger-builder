b06ac43dcd1319760c6227931f8d37e2d7f48fe5
ashleighmm <ashleigh@hoistdigital.co.uk>
2020-05-03 15:22:17 +0100

    Added logic to add ingredients onClick

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './BuildControls.css';
-import BuildControl from './BuildControl/BuildControl'
+import BuildControl from './BuildControl/BuildControl';
+
 const controls = [
     { label: 'Salad', type: 'salad' },
     { label: 'Cheese', type: 'cheese' },
@@ -11,7 +12,11 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
          {controls.map(ctrl => (
-             <BuildControl key={ctrl.label} label={ctrl.label} />
+             <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label} 
+                added={() => props.ingredientAdded(ctrl.type)}    
+            />
          ))}
     </div>
 );
