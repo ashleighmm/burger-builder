c8e8f316863372237312d0e1e592db2d749dd336
ashleighmm <ashleigh@hoistdigital.co.uk>
2020-05-18 22:33:58 +0100

    Fixed builder issue

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import classes from "./Burger.css";
+
+import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
-const burger = (props) => {
-    // transform an object of key pair values (from state BurgerBuilder) into an array of burger ingredients where the value of the object is important to decide how many ingredients are needed, the key is important for the type of ingredient specified
-    let transformedIngredients = Object.keys(props.ingredients)
-    .map(ingredientKey => {
-        return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
-            return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
-        });
-    }) // reduce flattens the array into a single value
-    .reduce((arr, el) => {
-        return arr.concat(el);
-    }, []);
-    // By adding reduce method, can get the length of the array according to the value set in each state ingredient, instewad of the array having empty elements, in which case you would always have a length of 4 even if set to 0 (salad:0,meat:0,cheese:0,bacon:0)
-    if(transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please pick some ingredients!</p>
+
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
     }
-    return(
+    return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
             {transformedIngredients}
@@ -24,4 +24,5 @@ const burger = (props) => {
         </div>
     );
 };
+
 export default burger;
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,30 @@
-import React, {Component} from 'react';
-import Aux from './../../../hoc/Aux';
-import Button from './../../UI/Button/Button';
+import React from 'react';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingredientKey => {
-            return <li key={ingredientKey}>
-                <span style={{textTransform:'capitalize'}}>
-                    {ingredientKey}</span>: {props.ingredients[ingredientKey]}
-                </li>
-        })
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = ( props ) => {
+    const ingredientSummary = Object.keys( props.ingredients )
+        .map( igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li> );
+        } );
+
     return (
         <Aux>
             <h3>Your Order</h3>
-            <p>Your burger contains the following ingredients:</p>
+            <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p>Total Price: {props.price}</p>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
-}
+    );
+};
 
 export default orderSummary;
